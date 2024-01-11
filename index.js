ac0975e82fb3d5a6bd4ccfaa61599ece2d95f7e4
AnthonyScott <anthony.scott1005@gmail.com>
2024-01-11 16:36:35 -0500

    Add a simple webserver running in Node

---
+++ index.js
@@ -0,0 +1,2 @@
+const express = require('express'); const app = express(); const port = 3000;// Route for the homepage
+app.get('/', (req, res) => { res.send('<h1>Hello, World from Express.js!</h1>'); }); app.listen(port, () => { console.log(`Server running at http://localhost:${port}/`); });
